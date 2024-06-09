38e7ff04c04febe1bdd27268b82b4ecc927a7534
Carla Mariana <carlamab2808@outlook.com>
2024-06-09 03:18:03 +0000

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return <h1> Sou linda e feliz! </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada - corrigindo com prettier");
+}
+
+export default Home;
